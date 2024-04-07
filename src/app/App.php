f8b8e1e497c63e0a7bb7c19de8ea46ac59419483
Arseny <20mineslend14@gmail.com>
2024-04-07 11:36:06 +0500

    create di scan components

--- src/app/App.php
+++ src/app/App.php
@@ -4,25 +4,35 @@ namespace csuPhp\Csu2024;
 
 use csuPhp\Csu2024\components\BaseComponent;
 use csuPhp\Csu2024\di\ComponentContainer;
+use csuPhp\Csu2024\request\RequestFactoryImpl;
+use csuPhp\Csu2024\response\ResponseFactoryImpl;
+use csuPhp\Csu2024\router\Router;
 use Exception;
-use ReflectionClass;
-use ReflectionException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class App extends BaseComponent
 {
     private ComponentContainer $container;
     private array $params;
 
-    protected function init(): void {
+    protected function init(): void
+    {
         if (!array_key_exists('components', $this->params)) {
             throw new Exception();
         }
         $this->container = new ComponentContainer($this->params['components']);
     }
 
-    public function run() {
-         $router = $this->container->get('router');
-//         $router->run();
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function run(): void
+    {
+        $router = $this->container->get('router');
+        $this->container->get('userController');
+//        $router->run();
     }
 
     public function __construct(array $params)
@@ -30,54 +40,69 @@ class App extends BaseComponent
         $this->params = $params;
         $this->scanComponents();
         parent::__construct();
-//        $this->injectComponent();
     }
 
-    private function scanComponents() {
-        // echo "SCAN COMPONENTS";
-        // $dir = scandir(__DIR__ . '/..');
-        // $this->scanDir(__DIR__ . '/..');
-        // echo "<pre>";
-        // print_r($dir);
-    }
-
-    private function scanDir($target) {
-        if(is_dir($target)){
-            $files = glob( $target . '*', GLOB_MARK );
-            foreach( $files as $file )
-            {
-                if(is_file($file)) {
-                    echo $file;
-                    $class = new ReflectionClass(str_replace('.php', '', $file));
-                }
-                $this->scanDir($file);
-            }
-        } 
-    }
-
-    /**
-     * @throws ReflectionException
-     */
-    private function injectComponent(): void {
-        $components = $this->params['components'];
-        foreach($components as $name => $class) {
-            $ref = new ReflectionClass($class);
-            $constructor = $ref->getConstructor();
-            $constructorParameters = $constructor->getParameters();
-            $params = [];
-            $index = 0;
-            foreach($constructorParameters as $param) {
-                $obj = new ($param->getType()->getName())();
-                $array = preg_split('\\', get_class($obj));
-                $getcls = end($array);
-                echo $getcls;
-                $paramName = lcfirst($getcls);
-                echo $paramName;
-                $component = $this->container->get($paramName);
-                $params[$index] = $component;
-                $index++;
-            }
-            new $ref($params);
+    private function scanComponents(): void
+    {
+        if (!isset($this->params['componentScan'])) {
+            $this->params['componentScan'] = __DIR__;
         }
+        $classes = $this->scanClasses($this->params['componentScan']);
+        $this->addComponentsToConfig($classes);
+        $this->addStaticComponents();
+    }
+
+    private function addStaticComponents(): void
+    {
+        $components = &$this->params['components'];
+        $components['router'] = Router::class;
+        $components['requestFactoryImpl'] = RequestFactoryImpl::class;
+        $components['responseFactoryImpl'] = ResponseFactoryImpl::class;
+    }
+
+    private function addComponentsToConfig(array $classes): void
+    {
+        if (!isset($this->params['components'])) {
+            $this->params['components'] = [];
+        }
+        if (!is_array($this->params['components'])) {
+            $this->params['components'] = [];
+        }
+        foreach ($classes as $_ => $class) {
+            $array = explode("\\", $class);
+            $name = lcfirst(end($array));
+            $this->params['components'][$name] = $class;
+        }
+    }
+
+    private function scanClasses($dir): array
+    {
+        $classes = [];
+
+        $files = scandir($dir);
+
+        foreach ($files as $file) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+            $path = $dir . '/' . $file;
+
+            if (is_dir($path)) {
+                $classes = array_merge($classes, $this->scanClasses($path));
+            } elseif ($file == 'App.php' || str_contains($path, 'components') || str_contains($path, 'di')) {
+                continue;
+            } else {
+                $content = file_get_contents($path);
+
+                if (preg_match_all('/\bclass\s+([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)\b/', $content, $matches)) {
+                    $name = preg_replace('/\.php/', '', $path);
+                    $name = preg_replace('#' . $this->params['componentScan'] . '#', __NAMESPACE__, $name);
+                    $name = preg_replace("#/#", "\\", $name);
+                    $classes[] = $name;
+                }
+            }
+        }
+
+        return $classes;
     }
 }

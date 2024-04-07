1abc305cd1bbd3a5a47f7fa05fcd681059c7de15
ReedKappa <goshaidze03@gmail.com>
2024-04-07 17:50:48 +0500

    Added AuthorizationMiddleWare and ContentTypeMiddleware

---
+++ core/router/src/middleware/ContentTypeMiddleWare.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace core\router\middleware;
+
+class ContentTypeMiddleWare implements IMiddleware
+{
+
+    public function handle(array $params): bool
+    {
+        $contentType = $params['Content-Type'] ?? 'application/json';
+        return $this->isJsonCorrect($contentType);
+    }
+
+    private function isJsonCorrect(mixed $contentType): bool
+    {
+        $json = file_get_contents($contentType);
+
+        $data = json_decode($json, true);
+
+        if ($data == null && json_last_error() !== JSON_ERROR_NONE) {
+            echo "Ошибка парсинга json элемента" . json_last_error_msg();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+}
